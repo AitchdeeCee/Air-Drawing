4a68677e10d311b60005cebb2b95cf4fa691f15e
Hung Che <55414511+Hung-Che@users.noreply.github.com>
2020-07-21 16:06:23 -0500

    update webcam.cpp

--- Webcam/Webcam.cpp
+++ Webcam/Webcam.cpp
@@ -1,216 +1,25 @@
 ﻿/*
 	My name is Hung Che and this is my code for my hand coordinates recognition
 	For info about installing OpenCV, visit: https://medium.com/@subwaymatch/adding-opencv-4-2-0-to-visual-studio-2019-project-in-windows-using-pre-built-binaries-93a851ed6141
-
-
-
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include <iostream>
-
-using namespace std;
-using namespace cv;
-
-
-//our sensitivity value to be used in the absdiff() function
-const static int SENSITIVITY_VALUE = 100;
-//size of blur used to smooth the intensity image output from absdiff() function
-const static int BLUR_SIZE = 10;
-//we'll have just one object to search for
-//and keep track of its position.
-int theObject[2] = { 0,0 };
-//bounding rectangle of the object, we will use the center of this as its position.
-Rect objectBoundingRectangle = Rect(0, 0, 0, 0);
-
-
-//int to string helper function
-string intToString(int number) {
-
-	//this function has a number input and string output
-	std::stringstream ss;
-	ss << number;
-	return ss.str();
-}
-
-void searchForMovement(Mat thresholdImage, Mat& cameraFeed) {
-	//notice how we use the '&' operator for objectDetected and cameraFeed. This is because we wish
-	//to take the values passed into the function and manipulate them, rather than just working with a copy.
-	//eg. we draw to the cameraFeed to be displayed in the main() function.
-	bool objectDetected = false;
-	Mat temp;
-	thresholdImage.copyTo(temp);
-	//these two vectors needed for output of findContours
-	vector< vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-	//find contours of filtered image using openCV findContours function
-	//findContours(temp,contours,hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_SIMPLE );// retrieves all contours
-	findContours(temp, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);// retrieves external contours
-
-	//if contours vector is not empty, we have found some objects
-	if (contours.size() > 0)objectDetected = true;
-	else objectDetected = false;
-
-	if (objectDetected) {
-		//the largest contour is found at the end of the contours vector
-		//we will simply assume that the biggest contour is the object we are looking for.
-		vector< vector<Point> > largestContourVec;
-		largestContourVec.push_back(contours.at(contours.size() - 1));
-		//make a bounding rectangle around the largest contour then find its centroid
-		//this will be the object's final estimated position.
-		objectBoundingRectangle = boundingRect(largestContourVec.at(0));
-		int xpos = objectBoundingRectangle.x + objectBoundingRectangle.width / 2;
-		int ypos = objectBoundingRectangle.y + objectBoundingRectangle.height / 2;
-
-		//update the objects positions by changing the 'theObject' array values
-		theObject[0] = xpos, theObject[1] = ypos;
-	}
-	//make some temp x and y variables so we dont have to type out so much
-	int x = theObject[0];
-	int y = theObject[1];
-
-	//draw some crosshairs around the object
-	circle(cameraFeed, Point(x, y), 20, Scalar(0, 255, 0), 2);
-	line(cameraFeed, Point(x, y), Point(x, y - 25), Scalar(0, 255, 0), 2);
-	line(cameraFeed, Point(x, y), Point(x, y + 25), Scalar(0, 255, 0), 2);
-	line(cameraFeed, Point(x, y), Point(x - 25, y), Scalar(0, 255, 0), 2);
-	line(cameraFeed, Point(x, y), Point(x + 25, y), Scalar(0, 255, 0), 2);
-
-	//write the position of the object to the screen
-	putText(cameraFeed, "Tracking object at (" + intToString(x) + "," + intToString(y) + ")", Point(x, y), 1, 1, Scalar(255, 0, 0), 2);
-	cout << "X: " << x/2 << " Y: " << y/2 << "\n";
-}
-
-int main(int argc, char** argv) {
-	
-	//some boolean variables for added functionality
-	bool objectDetected = false;
-	//these two can be toggled by pressing 'd' or 't'
-	bool debugMode = false;
-	bool trackingEnabled = false;
-	//pause and resume code
-	bool pause = false;
-	//set up the matrices that we will need
-	//the two frames we will be comparing
-	Mat frame1, frame2;
-	//their grayscale images (needed for absdiff() function)
-	Mat grayImage1, grayImage2;
-	//resulting difference image
-	Mat differenceImage;
-	//thresholded difference image (for use in findContours() function)
-	Mat thresholdImage;
-	//video capture object.
-	
-	VideoCapture cap(0);
-	if (cap.isOpened() == false)
-	{
-		cout << "Cannot open the video camera" << endl;
-		cin.get(); //wait for any key press
-		return -1;
-	}
-
-	double dWidth = cap.get(CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-	double dHeight = cap.get(CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
-
-	cout << "Resolution of the video : " << dWidth << " x " << dHeight << endl;
-	while (1) {
-		//read first frame
-		cap.read(frame1);
-		flip(frame1, frame1, 1);
-		//convert frame1 to gray scale for frame differencing
-		cv::cvtColor(frame1, grayImage1, COLOR_BGR2GRAY);
-		//copy second frame
-		cap.read(frame2);
-		flip(frame2, frame2, 1);
-		//convert frame2 to gray scale for frame differencing
-		cv::cvtColor(frame2, grayImage2, COLOR_BGR2GRAY);
-		//perform frame differencing with the sequential images. This will output an "intensity image"
-		//do not confuse this with a threshold image, we will need to perform thresholding afterwards.
-		cv::absdiff(grayImage1, grayImage2, differenceImage);
-		//threshold intensity image at a given sensitivity value
-		cv::threshold(differenceImage, thresholdImage, SENSITIVITY_VALUE, 255, THRESH_BINARY);
-		//show the difference image and threshold image
-		if (debugMode == true) {
-			cv::imshow("Difference Image", differenceImage);
-			cv::imshow("Threshold Image", thresholdImage);
-		}
-		else {
-			cv::destroyWindow("Difference Image");
-			cv::destroyWindow("Threshold Image");
-		}
-		//blur the image to get rid of the noise. This will output an intensity image
-		cv::blur(thresholdImage, thresholdImage, cv::Size(BLUR_SIZE, BLUR_SIZE));
-		//threshold again to obtain binary image from blur output
-		cv::threshold(thresholdImage, thresholdImage, SENSITIVITY_VALUE, 255, THRESH_BINARY);
-		if (debugMode == true) {
-			//show the threshold image after it's been "blurred"
-
-			imshow("Final Threshold Image", thresholdImage);
-
-		}
-		else {
-			//if not in debug mode, destroy the windows so we don't see them anymore
-			cv::destroyWindow("Final Threshold Image");
-		}
-
-		//if tracking enabled, search for contours in our thresholded image
-		if (trackingEnabled) {
-
-			searchForMovement(thresholdImage, frame1);
-		}
-
-		//show our captured frame
-		imshow("Frame1", frame1);
-		//check to see if a button has been pressed.
-		//this 10ms delay is necessary for proper operation of this program
-		//if removed, frames will not have enough time to referesh and a blank
-		//image will appear.
-		switch (waitKey(10)) {
-
-		case 27: //'esc' key has been pressed, exit program.
-			return 0;
-		case 116: //'t' has been pressed. this will toggle tracking
-			trackingEnabled = !trackingEnabled;
-			if (trackingEnabled == false) cout << "Tracking disabled." << endl;
-			else cout << "Tracking enabled." << endl;
-			break;
-		case 100: //'d' has been pressed. this will debug mode
-			debugMode = !debugMode;
-			if (debugMode == false) cout << "Debug mode disabled." << endl;
-			else cout << "Debug mode enabled." << endl;
-			break;
-		case 112: //'p' has been pressed. this will pause/resume the code.
-			pause = !pause;
-			if (pause == true) {
-				cout << "Code paused, press 'p' again to resume" << endl;
-				while (pause == true) {
-					//stay in this loop until
-					switch (waitKey()) {
-						//a switch statement inside a switch statement? Mind blown.
-					case 112:
-						//change pause back to false
-						pause = false;
-						cout << "Code Resumed" << endl;
-						break;
-					}
-				}
-			}
-		}
-	}
-
-	return 0;
-
-}
 */
 #include <sstream>
 #include <string>
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
+#include "Tserial.h"
+#include "Tserial.cpp"
 #include <iostream>
+
+const char* const port = "\\\\.\\COM6";
 
 using namespace cv;
 using namespace std;
+
+//Arduino
+int arduino_command;
+Tserial* arduino_com;
+
 //initial min and max HSV filter values.
 //these will be changed using trackbars
 int H_MIN = 54;
@@ -233,10 +42,7 @@ const string windowName1 = "HSV Image";
 const string windowName2 = "Thresholded Image";
 const string windowName3 = "After Morphological Operations";
 const string trackbarWindowName = "Trackbars";
-void on_trackbar(int, void*)
-{
-
-}
+void on_trackbar(int, void*){}
 string intToString(int number) {
 
 
@@ -348,6 +154,8 @@ void trackFilteredObject(int& x, int& y, Mat threshold, Mat& cameraFeed) {
 					objectFound = true;
 					refArea = area;
 					cout << "X: " << x / 2 << " Y: " << y / 2 << "\n";
+					arduino_com->sendChar(x);
+					arduino_com->sendChar(y);
 				}
 				else objectFound = false;
 
@@ -364,8 +172,16 @@ void trackFilteredObject(int& x, int& y, Mat threshold, Mat& cameraFeed) {
 		else putText(cameraFeed, "TOO MUCH NOISE! ADJUST FILTER", Point(0, 50), 1, 2, Scalar(0, 0, 255), 2);
 	}
 }
+
 int main(int argc, char* argv[])
 {
+	
+	// serial to Arduino setup 
+	arduino_com = new Tserial();
+	if (arduino_com != 0) {
+		arduino_com->connect(const_cast<char*>(port), 9600, spNONE);
+	}
+	// serial to Arduino setup 
 	//some boolean variables for different functionality within this
 	//program
 	bool trackObjects = true;
@@ -418,7 +234,15 @@ int main(int argc, char* argv[])
 
 		//delay 30ms so that screen can refresh.
 		//image will not appear without this waitKey() command
-		waitKey(30);
+		if (waitKey(30) == 27) {
+			return 0;
+
+		}
 	}
+	// Serial to Arduino - shutdown
+	arduino_com->disconnect();
+	delete arduino_com;
+	arduino_com = 0;
+	// Serial to Arduino - shutdown
 	return 0;
 }
